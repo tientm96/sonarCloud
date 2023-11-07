d72a413a4128ae74b6c5104017d412341e8d1cb7
UPHEADS-TIENTRA\TienTran-Upheads <tien.tran@upheads.no>
2023-11-07 19:19:38 +0700

    sonar pipeline test

--- SonarCloud/Controllers/WeatherForecastController.cs
+++ SonarCloud/Controllers/WeatherForecastController.cs
@@ -29,5 +29,90 @@ namespace SonarCloud.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            NullReferenceExample();
+
+            NoUsingExample();
+
+            InfiniteLoopExample();
+            DeadCodeExample();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        private static int NullReferenceExample()
+        {
+            int? number = null;
+            return number.Value;
+        }
+
+        private static void NoUsingExample()
+        {
+            FileStream fileStream = new FileStream("example.txt", FileMode.Open);
+        }
+
+        public static void InfiniteLoopExample()
+        {
+            while (true)
+            {
+                Console.WriteLine("This is an infinite loop.");
+            }
+        }
+
+        public static void DeadCodeExample()
+        {
+            int a = 5;
+            int b = 10;
+
+            if (a > b)
+            {
+                Console.WriteLine("This code will never run.");
+            }
+            else
+            {
+                Console.WriteLine("This code is always executed.");
+            }
+        }
+
+        public static string OtherCases()
+        {
+            string url = "scheme://user:Admin123@domain.com";
+
+            return url;
+        }
+
+        public static void ShouldNotBeEmpty()
+        {
+        }
+
+        //"async" methods should not return "void"
+        public static async void DoSthAsync()
+        {
+        }
+
+        //Loops with at most one iteration should be refactored
+        public object Method(IEnumerable<object> items)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                break; // Noncompliant: loop only executes once
+            }
+
+            foreach (object item in items)
+            {
+                return item; // Noncompliant: loop only executes once
+            }
+            return null;
+        }
     }
 }
