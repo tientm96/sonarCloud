09ebbf9f18c1e18550bcba93c0409cc42c3b7679
UPHEADS-TIENTRA\TienTran-Upheads <tien.tran@upheads.no>
2023-11-08 12:17:01 +0700

    revert code

--- SonarCloud/Controllers/WeatherForecastController.cs
+++ SonarCloud/Controllers/WeatherForecastController.cs
@@ -57,7 +57,7 @@ namespace SonarCloud.Controllers
 
         private static void NoUsingExample()
         {
-            _ = new FileStream("example.txt", FileMode.Open);
+            FileStream fileStream = new FileStream("example.txt", FileMode.Open);
         }
 
         public static void InfiniteLoopExample()
